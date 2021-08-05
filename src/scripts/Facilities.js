afd9398b3458d65cce7c12cf04f95f4e02a63c91
kyliepost <kylie.postlethwaite00@gmail.com>
2021-08-05 15:24:26 -0500

    functions galore

--- src/scripts/Facilities.js
+++ src/scripts/Facilities.js
@@ -7,3 +7,22 @@
 
 //click new facility button, previously rendered facility's list should disappear
 
+import { getFacilities } from "./database.js"
+
+const facilities = getFacilities()
+
+export const FacilityList = () => {
+    
+    let html = ""
+
+    const facilitiesArray = facilities.map(
+        (facility) => {
+            return `<div><button name="portal-1" value="${facility.id}"> ${facility.name} <br></br> Click to access minerals </button></div>`
+
+        }
+
+    ) 
+    html += facilitiesArray.join("")
+    return html
+}
+
