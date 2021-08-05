e496dae84503b1c15201f37a7122edc0a1087338
CassieTesauro <cassie.tesauro@gmail.com>
2021-08-05 14:19:23 -0500

    resolved merge issues

--- src/scripts/BeMine.js
+++ src/scripts/BeMine.js
@@ -1,35 +1,46 @@
 import { Minerals } from "./Minerals.js"
 import { Governors } from "./Governors.js"
-
-
+import { Facilities } from "./Facilities.js"
+import { Colonies } from "./Colonies.js"
+import { availableResources } from "./database.js"
 
 export const BeMine = () => {
     return `
     <h1>Active Mining Facilities</h1>
+
     <article class="facilities">
-        <div class="facility--1">
-          <h2>Facility 1</h2>
-          <button id="btn-facility-1">Choose Minerals</button>
-          <div id="minerals_1"></div>
+       
+        <div class="facilityList">
+            ${facilityList()}
         </div>
-        <div class="facility--2">
-            <h2>Facility 2</h2>
-            ${Minerals()}
-        </div>
-        <div class="facility--3">
-            <h2>Facility 3</h2>
-            ${Minerals()}
-        </div>
-        <div class="facility--4">
-            <h2>Facility 4</h2>
-            ${Minerals()}
-        </div>
+
     </article>
+
+    <article class="orderBuilder"> 
+    <h2>Chosen Minerals</h2>
+        ${orderBuilder()}
+    </article>
+
+    <article>
+     <button id="orderButton">Purchase All Minerals</button>
+    </article>
+
+    <article class="governorsAndAvailableResources">
     <form class="governors">
         ${Governors()}
         </select>
         <br><br>
         <input type="submit" value="Submit">
     </form>
+    <div class="availableResources">
+    <h2>Available Resources for Colony</h2>
+    ${}
+    </div>
+
+    </article>
     `
 }
+
+    
+
+
