d53bd678df7ba3a49b208cf8b0e33cb9f97f1a86
CassieTesauro <cassie.tesauro@gmail.com>
2021-08-05 13:53:23 -0500

    modified beMine.js

---
+++ src/scripts/BeMine.js
@@ -0,0 +1,46 @@
+import { Minerals } from "./Minerals.js"
+import { Governors } from "./Governors.js"
+import { Facilities } from "./Facilities.js"
+import { Colonies } from "./Colonies.js"
+import { availableResources } from "./database.js"
+
+
+
+
+export const BeMine = () => {
+    return `
+    <h1>Active Mining Facilities</h1>
+
+    <article class="facilities">
+       
+        <div class="facilityList">
+            ${facilityList()}
+        </div>
+
+    </article>
+
+    <article class="orderBuilder"> 
+    <h2>Chosen Minerals</h2>
+        ${orderBuilder()}
+    </article>
+
+    <article>
+     <button id="orderButton">Purchase All Minerals</button>
+    </article>
+
+    <article class="governorsAndAvailableResources">
+    <form class="governors">
+        ${Governors()}
+        </select>
+        <br><br>
+        <input type="submit" value="Submit">
+    </form>
+    
+    <div class="availableResources">
+    <h2>Available Resources for Colony</h2>
+    ${}
+    </div>
+
+    </article>
+    `
+}
---
+++ src/scripts/Facilities.js
@@ -0,0 +1,9 @@
+//facilities active only enabled when gov is chosen
+
+//when choose facility --> current mineral names/amounts displays
+//If mineral amount = 0, don't display
+
+//HOW DOES IT DISPLAY? render entire radio buttion list beneath button when clicked.
+
+//click new facility button, previously rendered facility's list should disappear
+
