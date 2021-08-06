135b7534821b9a8a288efac71859735cf686221a
CassieTesauro <cassie.tesauro@gmail.com>
2021-08-06 14:28:56 -0500

    defined function to render purchase html

--- src/scripts/ChosenOptions.js
+++ src/scripts/ChosenOptions.js
@@ -1,2 +1,11 @@
 //refer to cars-r-us orders.js
 //need to set up the html that is triggered by clicking purchase button
+
+
+
+//  C/K this function is called in BeMine.js to render the completed order's html.  should be type and amount.
+
+const ColonyAvailableResources = (permanentObjectState) => { 
+   // let html = "<ul>" 
+    return `<ul>${availableResources.selectedMinerals}</ul>`
+}
--- src/scripts/ColoniesMinerals.js
+++ src/scripts/ColoniesMinerals.js
@@ -4,6 +4,8 @@
         const newOrder = {...database.orderBuilder}
         const lastIndex = database.availableResources.length - 1
         newOrder.id = database.availableResources[lastIndex].id + 1
+        ListItemsArray.mineral.stock -= 1 //subtracting from facility
+        //make sure amount from the tranasient data and name is diplayed in event
         database.availableResources.push(newOrder)
         database.orderBuilder = {}
         document.dispatchEvent(new CustomEvent("stateChanged"))
@@ -13,3 +15,8 @@
 
 
 //<article class="governorsColonyAvailableResources">   <div class="ColonyAvailableResources">< /div>    </article>
+
+
+chosenGovernor: integer
+ selectedFacility: integer
+  selectedMinerals: [{type: X, amount: 1}]
