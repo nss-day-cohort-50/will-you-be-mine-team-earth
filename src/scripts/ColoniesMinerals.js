bcedc7d1a5cc4273379ba12938956a98366ef86b
ts <shanksleslietrey@gmail.com>
2021-08-10 14:21:14 -0500

    added goverColonyId to purchased minerals html

--- src/scripts/ColoniesMinerals.js
+++ src/scripts/ColoniesMinerals.js
@@ -1,4 +1,4 @@
-import { getColoniesMinerals, getOrderBuilder } from "./database.js"
+import { getColoniesMinerals, getOrderBuilder, addPurchase, getColonies, getGovernors } from "./database.js"
 
 
 
@@ -9,6 +9,7 @@ document.addEventListener(
     (event) => {
         if (event.target.id === "purchaseButton") {
             window.alert("purchase button clicked")
+            addPurchase()
             
         }
     }
@@ -16,29 +17,25 @@ document.addEventListener(
 
 const orderBuilder = getOrderBuilder()
 
-//a fucntion that matches the name of the mineral to the id of the permanent state
-// const findPurchasedMineral = () => {
-//     const colonyAvailableResources = getColonyAvailableResources()
-//     let purchasedMineral = ""
-//     for (const mineral of colonyAvailableResources) {
-//         if (mineral.id === orderBuilder.colonyAvailableResourcesId) {
-//             purchasedMineral = mineral.type
-//         }
-//     }
+const findChosenColonyName = () => {
+    const colonies = getColonies()
+    let chosenColony = ""
+    for (const colony of colonies) {
+        if (colony.id === orderBuilder.colonyId) {
+            chosenColony = colony.name
+        }
+    }
 
-//     return purchasedMineral
-// }
+    return chosenColony
+}
 
 export const PurchasedMinerals = () => {
+    const chosenColonyName = findChosenColonyName()
     const coloniesMinerals = getColoniesMinerals()
-    let html = ``
-        const colonyMineralsNameArray = coloniesMinerals.map(
-            (colonyMineralsName) => {
-                return `
-                <h2>${colonyMineralsName.colonyName} Minerals</h2>
-                <ul>`
-            } 
-         ) 
+    let html = "<h2> Colony Minerals</h2>"
+    if (orderBuilder.governorId > 0){
+         html = `<h2> ${chosenColonyName} Minerals </h2>
+        </ul>`
          const colonyMineralsTypeArray = coloniesMinerals.map(
             (colonyMineralsType) => {
                 return `<li>
@@ -46,9 +43,9 @@ export const PurchasedMinerals = () => {
                 </li>`
             } 
          ) 
-    html += colonyMineralsNameArray 
-    html += colonyMineralsTypeArray.join("")
-    html += `</ul>`
+        html += colonyMineralsTypeArray.join("")
+        html += `</ul>`
+    }
     return html
 }
 
